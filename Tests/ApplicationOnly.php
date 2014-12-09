79366f3dc856de7adc524bbca3ca8f537cb831fd
Ricardo Pereira <ricardo.per@gmail.com>
2014-12-09 12:30:10 +0100

    Add Some Tests (NOT Unit Tests)

---
+++ Tests/ApplicationOnly.php
@@ -0,0 +1,86 @@
+<?php
+
+/**
+ * TwitterOAuth - https://github.com/ricardoper/TwitterOAuth
+ * PHP library to communicate with Twitter OAuth API version 1.1
+ *
+ * @author Ricardo Pereira <github@ricardopereira.es>
+ * @copyright 2014
+ */
+
+require __DIR__ . '/../../../vendor/autoload.php';
+
+use TwitterOAuth\Auth\ApplicationOnlyAuth;
+use TwitterOAuth\Serializer\ArraySerializer;
+
+
+date_default_timezone_set('UTC');
+
+header('Content-Type: text/html; charset=utf-8');
+
+?>
+
+<script src="//code.jquery.com/jquery-1.11.0.min.js"></script>
+<script>
+    window.onload = function(){
+        $('pre.array')
+            .addClass('closed')
+            .click(function(){
+                $(this).toggleClass('closed');
+            });
+    }
+</script>
+<style>
+    pre{overflow:hidden;}
+    .strip{margin-left:5px;width:90%;vertical-align:top;display:inline-block;word-wrap:break-word;}
+    .closed{height:84px;cursor:pointer;}
+</style>
+
+<?php
+
+$credentials = array(
+    'consumer_key' => 'xvz1evFS4wEEPTGEFPHBog',
+    'consumer_secret' => 'L8qq9PZyRg6ieKGEKhZolGC0vJWLw8iEJ88DRdyOg',
+);
+
+$auth = new ApplicationOnlyAuth($credentials, new ArraySerializer());
+
+// ==== ==== ==== //
+
+$params = array(
+    'screen_name' => 'ricard0per',
+    'count' => 3,
+    'exclude_replies' => true,
+);
+
+$response = $auth->get('statuses/user_timeline', $params);
+
+echo '<strong>statuses/user_timeline</strong><br />';
+echo '<pre class="array">'; print_r($auth->getHeaders()); echo '</pre>';
+echo '<pre class="array">'; print_r($response); echo '</pre><hr />';
+
+// ==== ==== ==== //
+
+$params = array(
+    'q' => '#php',
+    'count' => 3,
+);
+
+$response = $auth->get('search/tweets', $params);
+
+echo '<strong>search/tweets</strong><br />';
+echo '<pre class="array">'; print_r($auth->getHeaders()); echo '</pre>';
+echo '<pre class="array">'; print_r($response); echo '</pre><hr />';
+
+// ==== ==== ==== //
+
+$params = array(
+    'screen_name' => 'ricard0per',
+    'count' => 10,
+);
+
+$response = $auth->get('followers/ids', $params);
+
+echo '<strong>followers/ids</strong><br />';
+echo '<pre class="array">'; print_r($auth->getHeaders()); echo '</pre>';
+echo '<pre class="array">'; print_r($response); echo '</pre><hr />';
